a97db218b501f04fcb4e5d91ed233e1a2809a5be
jake roth <email": null>
2015-10-02 19:27:01 -0400

    made the signout and the sign in and login pages all of which are not really CRUD oriented because there are new users but there are also sign in users which arent really creating or editing

--- app/controllers/user/user.rb
+++ app/controllers/user/user.rb
@@ -3,7 +3,7 @@ get '/signup' do
   erb :'user/signup'
 end
 
-get '/signedin' do
+put '/signedin' do
   @user = User.new(params[:user])
   @user.password = params[:password]
   if @user.save
@@ -15,5 +15,23 @@ get '/signedin' do
   end
 end
 
+get '/signout' do
+  session[:user_id] = nil
+  redirect "/decks"
+end
 
+get '/signin' do
+  @user = User.new
+  erb :'user/signin'
+end
+
+put '/login' do
+  @user = User.find_by(email: params[:email])
+  if @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect "/decks"
+  else
+    @errors = ["we didnt find a match check your email and password again"]
+    erb :'user/signin'
+  end
 end
