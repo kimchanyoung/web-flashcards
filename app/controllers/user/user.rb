814e108f9b15db79bef30b59da5d79626088c952
jake roth <email": null>
2015-10-02 19:25:23 -0400

    made the new route or the signed in route

--- app/controllers/user/user.rb
+++ app/controllers/user/user.rb
@@ -3,5 +3,17 @@ get '/signup' do
   erb :'user/signup'
 end
 
+get '/signedin' do
+  @user = User.new(params[:user])
+  @user.password = params[:password]
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/decks"
+  else
+    @errors = @user.errors.full_messages
+    erb :'user/signup'
+  end
+end
+
 
 end
