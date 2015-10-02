fb6d1e664478815e35b581b0d52cd72ee34ff2d2
LAURA EVANS <LKSEVANS12@GMAIL.com>
2015-10-02 19:00:32 -0400

    removed instance variable, and changed parameter name

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,8 +2,8 @@ class Card < ActiveRecord::Base
   belongs_to :deck
   has_many :guesses
 
-  def correct?(params)
-    if @term == params
+  def correct?(user_guess)
+    if term == user_guess
       true
     else
       false
