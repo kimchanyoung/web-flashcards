d6754b19e2f109cebdebb1a562f45cf9dd4fa927
Chanyoung Kim <email": null>
2015-10-02 17:14:45 -0400

    Refactor logic; remove card logic

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,42 +3,28 @@ class Deck < ActiveRecord::Base
 
   attr_accessor :cards_to_go
   attr_accessor :cards_to_repeat
-  attr_accessor :not_done
   attr_accessor :current_card_index
 
   def initialize()
     @cards_to_go = cards.clone
     @cards_to_go.shuffle!
     @cards_to_repeat = []
-    @not_done = true
-    @current_card_index = 0
   end
 
   def current_card
-    cards_to_go[current_card_index]
+    if cards_to_go.empty?
+      self.cards_to_go = cards_to_repeat.clone.shuffle
+      cards_to_repeat.clear
+    end
+    cards_to_go.pop
   end
 
-  def guess(attempt)
-    if current_card.guess(attempt)
-      true
-    else
-      cards_to_repeat << current_card
-      false
-    end
+  def done?
+    cards_to_go.empty? && cards_to_repeat.empty?
   end
 
-  def flip_card
-    if current_card_index == @cards_to_go.size
-      if cards_to_repeat.empty?
-        self.not_done = true
-      else
-        self.cards_to_go = cards_to_repeat.clone.shuffle
-        self.current_card_index = 0
-        cards_to_repeat.clear
-      end
-    else
-      self.current_card_index += 1
-    end
+  def repeat_card(card)
+    cards_to_repeat << card
   end
 
 end
