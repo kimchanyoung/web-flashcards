7ddd1a7491bf8b11aa6bd8bc3d76bca2363b4c50
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-03 00:37:42 -0400

    Add done_cards method, and add relations between cards and rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,10 @@
 class Round < ActiveRecord::Base
   belongs_to :user
   has_many :guesses
+  has_many :cards, through: :guesses
+
+  def done_cards
+    # cards.where(id: guesses.pluck(:card_id))
+    guesses.map{|guess| guess.card}
+  end
 end
