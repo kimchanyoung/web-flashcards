a5bed9574eb52b4c8156bdbc28ce2ecfa57b47d7
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-03 00:55:32 -0400

    Remove faulty deck logic

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,30 +1,3 @@
 class Deck < ActiveRecord::Base
   has_many :cards
-
-  attr_accessor :cards_to_go
-  attr_accessor :cards_to_repeat
-  attr_accessor :current_card_index
-
-  def initialize()
-    @cards_to_go = cards.clone
-    @cards_to_go.shuffle!
-    @cards_to_repeat = []
-  end
-
-  def current_card
-    if cards_to_go.empty?
-      self.cards_to_go = cards_to_repeat.clone.shuffle
-      cards_to_repeat.clear
-    end
-    cards_to_go.pop
-  end
-
-  def done?
-    cards_to_go.empty? && cards_to_repeat.empty?
-  end
-
-  def repeat_card(card)
-    cards_to_repeat << card
-  end
-
 end
