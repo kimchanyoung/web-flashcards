8584c3887083570cea49861b47221aaf985cdf6a
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-03 02:12:47 -0400

    Add route to select random card, plus a route to the round show page upon completion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@ Card.create!  term: 'jake', definition: 'who is the smartest?', deck_id: 66
 Card.create!  term: 'jake', definition: 'who is the has a great fashion sense?', deck_id: 66
 Card.create!  term: 'jake', definition: 'who is the worst at spelling?', deck_id: 66
 Card.create!  term: 'jake', definition: 'who do you love?', deck_id: 66
+
+Deck.create(id: 7, name: "The Answer Is Always Blue")
+Card.create(term: 'Blue', defintion: 'Which color starts with B and rhymes with glue?', deck_id: 7)
+Card.create(term: 'Blue', definition: 'What is always the answer?', deck_id: 7)
+Card.create(term: 'Blue', definition: 'Which color compliments orange?', deck_id: 7)
+Card.create(term: 'Blue', definition: 'What color is the sky?', deck_id: 7)
