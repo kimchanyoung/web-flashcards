d6e370222f7db41edfc1464db670e2fd9c85ed82
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-03 02:55:51 -0400

    Add is_correct boolean field to the guess database

--- db/migrate/20151002153458_create_guesses.rb
+++ db/migrate/20151002153458_create_guesses.rb
@@ -3,6 +3,7 @@ class CreateGuesses < ActiveRecord::Migration
     create_table :guesses do |t|
       t.integer :card_id, null: false
       t.integer :round_id, null: false
+      t.boolean :is_correct, null: false
       t.timestamps null: false
     end
   end
