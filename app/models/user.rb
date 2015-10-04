0221b856feb3d93143852a153a417f26446116bc
jake roth <email": null>
2015-10-04 14:41:19 -0400

    added a custom email format validation method and check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ActiveRecord::Base
   include BCrypt
 
+  has_many :rounds
+
   validates :username, :email, :password_hash, presence: true
   validates_uniqueness_of :email
+  validate :valid_email_format
 
-  has_many :rounds
+  def valid_email_format
+    if (email =~ /^[a-zA-Z0-9]*@.*\.com$/).nil?
+      errors.add(:email_error, ":you need a format XXXXXX@example.com where the Xs are numbers or letters")
+    end
+  end
+
 
   def password
     @password ||= Password.new(password_hash)
