ebe7c27a5a55145bdce4fdf8883c8fbe5d122bd0
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-04 14:49:29 -0400

    Seed's a Game of Thrones deck into the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@ Card.create!  term: 'jake', definition: 'who is the smartest?', deck_id: 66
 Card.create!  term: 'jake', definition: 'who is the has a great fashion sense?', deck_id: 66
 Card.create!  term: 'jake', definition: 'who is the worst at spelling?', deck_id: 66
 Card.create!  term: 'jake', definition: 'who do you love?', deck_id: 66
+
+
+Deck.create!(id: 1, name: "Game of Thrones Trivia")
+
+Card.create!(term: 'stableboy',
+             definition: "What was Hodor's job at Winterfell before he became Bran's companion?",
+             deck_id: 1)
+Card.create!(term: 'targaryen',
+             definition: "What house is Maester Aemon from?",
+             deck_id: 1)
+Card.create!(term: 'lady',
+             definition: "Which direwolf is Ned forced to put down on their way to King's Landing?",
+             deck_id: 1)
+Card.create!(term: 'riverrun',
+             definition: "Where was Robb Stark born?",
+             deck_id: 1)
+Card.create!(term: 'cersei',
+             definition: "Which character said, 'In the Game of Thrones, you win or you die'?",
+             deck_id: 1)
+Card.create!(term: "titan's daughter",
+             definition: "What is the name of the ship that takes Arya to Braavos?",
+             deck_id: 1)
+Card.create!(term: 'summer',
+             definition: "What is the name of Bran's direwolf?",
+             deck_id: 1)
+Card.create!(term: 'sapphire isle',
+             definition: "What is the nickname for the Tarth island that Brienne is from?",
+             deck_id: 1)
+
