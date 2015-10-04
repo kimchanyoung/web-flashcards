989d3288c3766a5f924c99c9d08aee33a8993369
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-04 12:48:50 -0400

    Moves new round and guest user creation to deck controller

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,4 +1,4 @@
-get '/decks' do 
+get '/decks' do
   @decks = Deck.all
   erb :'deck/index'
 end
@@ -18,8 +18,19 @@ post '/decks' do
   end
 end
 
-get '/decks/:deck_id' do 
+get '/decks/:deck_id' do
   @deck = Deck.find(params[:deck_id])
+
+  unless session[:user_id]
+    user = User.create(username: "guest", password: "guest", email: "guest@email.com") #=> need to delete the guest user from table at end of round.
+
+    session[:user_id] = user.id
+  end
+
+  new_round = Round.create(user_id: session[:user_id])
+
+  session[:round_id] = new_round.id
+
   erb :'deck/show'
 end
 
