f56675bbd56444598fcebd5e1f4588742923a6c0
jake roth <email": null>
2015-10-04 14:21:34 -0400

    added the round routes to make a new round and then direct to the first random card show page

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -2,6 +2,13 @@ get '/rounds' do
   "hi there"
 end
 
+get 'round/new' do
+  new_round = Round.create(user_id: session[:user_id])
+  session[:round_id] = new_round.id
+  @deck = Deck.find(session[:deck_id])
+  redirect "/cards/#{@deck.cards.sample.id}"
+end
+
 get '/rounds/:round_id' do |round_id|
   @round = Round.find(round_id)
   @deck = Deck.find(session[:deck_id])
